e93a7dc2efa0f8cfed506965207e497dd7f43a73
Zhiyin Li <lizh21nv@student.ju.se>
2024-09-03 16:46:11 +0200

    let

--- changefolder/line.js
+++ changefolder/line.js
@@ -1,31 +1,31 @@
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
 
-var size = window.innerWidth;
-var dpr = window.devicePixelRatio;
+let size = window.innerWidth;
+let dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
 context.scale(dpr, dpr);
 
-var cols = 23;
-var rows = 16;
-var days = 365;
+let cols = 23;
+let rows = 16;
+let days = 365;
 
-var gridw = size * 0.9;
-var gridh = size * 0.7;
-var cellw = gridw / cols;
-var cellh = gridh / rows;
-var margx = (size - gridw) * 0.5;
-var margy = (size - gridh) * 0.5;
+let gridw = size * 0.9;
+let gridh = size * 0.7;
+let cellw = gridw / cols;
+let cellh = gridh / rows;
+let margx = (size - gridw) * 0.5;
+let margy = (size - gridh) * 0.5;
 
 for (let i = 0; i < days; i++) {
-  var col = Math.floor(i / rows);
-  var row = i % rows;
+    let col = Math.floor(i / rows);
+    let row = i % rows;
 
-  var x = margx + col * cellw;
-  var y = margy + row * cellh;
-  var w = 2;
-  var h = 30;
+    let x = margx + col * cellw;
+    let y = margy + row * cellh;
+    let w = 2;
+    let h = 30;
   
   context.save();
   context.translate(x, y);
@@ -36,12 +36,12 @@ for (let i = 0; i < days; i++) {
    
   context.translate(cellw * 0.5, cellh * 0.5);
 
-  var phi = (i / days) * Math.PI;
-  var theta = Math.sin(phi) * Math.PI * 0.45 + 0.85;
+  let phi = (i / days) * Math.PI;
+  let theta = Math.sin(phi) * Math.PI * 0.45 + 0.85;
 
   context.rotate(theta);
    
-  var scale = Math.abs(Math.cos(phi)) * 2 + 1;
+  let scale = Math.abs(Math.cos(phi)) * 2 + 1;
 
   context.scale(scale, 1);
    
