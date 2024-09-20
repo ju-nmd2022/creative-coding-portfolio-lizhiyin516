fb3feae32219acc09ae19a3fabaebfb96a2a5644
Zhiyin Li <lizh21nv@student.ju.se>
2024-09-20 15:27:53 +0200

    complex

---
+++ changefolder/complex.js
@@ -0,0 +1,54 @@
+rotation_speed = 0;
+
+function setup() {
+  createCanvas(1000, 600);
+  noStroke();
+  background("#005");
+  h = height / 2;
+ w = width / 2;
+}
+
+function draw() {
+  for (y = 0; y < h; y += 6) {
+    c = lerpColor(color("#005"), color("#22489D"), y / h);
+    fill(c);
+    square(y, y, width - 2 * y);
+  }
+ push()
+  fill("#28C752");
+  for (i = 6000; i > 0; i--) {
+    x = w + sin(i + rotation_speed) * tan(i * i ) * 250;
+    y = h + 250 * cos(i * i);
+    circle(x, y, 2);
+  }
+ pop()
+ 
+ push()
+  fill("#28C7BD");
+  for (i = 4000; i > 0; i--) {
+    x = w - cos(i + rotation_speed) * sin(i * i) * 200;
+    y = h - 200 * sin(i );
+    circle(x, y, 2);
+  }
+ pop()
+ 
+ push()
+  fill("#D25D69");
+  for (i = 4000; i > 0; i--) {
+    x = w + sin(i + rotation_speed) * sin(i * i) * 180;
+    y = h + 180 * cos(i);
+    circle(x, y, 2);
+  }
+ pop()
+ 
+ push()
+ // fill("#D225E1");
+ // for (i = 4000; i > 0; i--) {
+ // x = mouseX + sin(i + rotation_speed) * sin(i * i) * 150 * noise(sin(i + rotation_speed) * sin(i * i) + i);
+ // y = mouseY + 150 * cos(i * i) * noise(cos(i * i) + i);
+ // circle(x, y, 2);
+ // }
+ // pop()
+ 
+  rotation_speed += 0.001;
+}
